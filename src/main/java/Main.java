25325cfc0445e89af3df507532cc0345cf86217e
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-07-27 16:02:41 +0600

    till da lists

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
-import type_importance.LinkAndPrimitive;
-
 public class Main {
     public static void main(String[] args) {
-        LinkAndPrimitive linkAndPrimitive = new LinkAndPrimitive();
-        linkAndPrimitive.example();
     }
 }
---
+++ src/main/java/primitive_operations/DifferentTypeOperations.java
@@ -0,0 +1,13 @@
+package primitive_operations;
+
+public class DifferentTypeOperations {
+    public void example() {
+        double a = 120.75;
+        double b = 11.12;
+        System.out.println(a % b);
+
+        int numberInt = 123;
+        double numberDouble = 10.5;
+        System.out.println(numberInt / numberDouble);
+    }
+}
