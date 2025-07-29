1816ecfded2b17d57cc3d304be22aa89ea705c1c
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-07-29 17:53:41 +0600

    no accounting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
+import accounting.Accounting;
+
 public class Main {
     public static void main(String[] args) {
+        Accounting accounting = new Accounting();
+        accounting.example();
     }
 }
--- src/main/java/accounting/Accounting.java
+++ src/main/java/accounting/Accounting.java
@@ -1,4 +1,30 @@
 package accounting;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Accounting {
+    Scanner scanner =  new Scanner(System.in);
+    public void example() {
+//        int userInput = scanner.nextInt();
+//        scanner.nextLine();
+//        while (userInput != 0) {
+//
+//        }
+          String[] lines = readFileContents("D:\\programming\\yandex_practicum\\src\\main\\java\\accounting\\m.202101.csv").split("\n");
+          for (String line : lines) {
+              System.out.println(line);
+              System.out.println("new line");
+          }
+    }
+
+    private String readFileContents(String path) {
+        try {
+            return Files.readString(Path.of(path));
+        } catch (IOException e) {
+            return null;
+        }
+    }
 }
