61e5020f3e84fe8f079c416dae5c2b5f56312785
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-08-04 12:14:34 +0600

    manager class a bit

--- src/main/java/sprint2/final_project/FinalProject.java
+++ src/main/java/sprint2/final_project/FinalProject.java
@@ -1,7 +1,24 @@
 package sprint2.final_project;
+import java.util.Scanner;
 
 public class FinalProject {
+    Scanner scanner = new Scanner(System.in);
     public void example() {
         int taskNumber = 1;
+        int userInput = scanner.nextInt();
+        scanner.nextLine();
+        while (userInput != 0) {
+
+        }
+    }
+
+    public void printOptions() {
+        System.out.println("Чтобы создать задачу нажмите 1");
+        System.out.println("Чтобы изменить задачу нажмите 2");
+        System.out.println("Чтобы вывести список задач нажмите 3");
+        System.out.println("Чтобы получить задачу по идентификатору нажмите 4");
+        System.out.println("Чтобы удалить задачу по идентификатору нажмите 5");
+        System.out.println("Чтобы удалить все задачи нажмите 6");
+        System.out.println("Чтобы получить все подзадачи эпика нажмите 7");
     }
 }
