730844b2c0787125e2074376535865d4337e1025
Raiimkulov Bekzat <raiimkulov.bekzat@gmail.com>
2025-08-13 15:52:27 +0600

    check

--- src/main/java/sprint4/SortAlgorithm.java
+++ src/main/java/sprint4/SortAlgorithm.java
@@ -2,6 +2,25 @@ package sprint4;
 
 public class SortAlgorithm {
     public static void main(String[] args) {
+        int[] array = {1, 3, 464, 334, 434, 1, 2, 3, 4343, 10, 43, 2};
+        insertionSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 
+    static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+
+            array[j + 1] = key;
+        }
     }
 }
