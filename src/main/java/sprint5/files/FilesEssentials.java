84824e6484d98305fdb1e86f632cdd068a2fc666
Bekzt Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-08-23 00:52:33 +0600

    files

---
+++ src/main/java/sprint5/files/FilesEssentials.java
@@ -0,0 +1,10 @@
+package sprint5.files;
+
+import java.io.File;
+
+public class FilesEssentials {
+    public static void main(String[] args) {
+        File dir = new File("/home/osolenye/programming/yandex_practicum/src/main/java/sprint5/files");
+
+    }
+}
