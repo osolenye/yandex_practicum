88362de0c58ce23f15fec3b3a2a3199546a3f7df
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-08-24 13:40:21 +0600

    done saving method

--- src/main/java/sprint5/final_project/FileBackedTaskManager.java
+++ src/main/java/sprint5/final_project/FileBackedTaskManager.java
@@ -88,44 +88,41 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
         if (task.getClass() == Task.class) {
             writeTaskToDB(task);
         } else if (task.getClass() == EpicTask.class) {
-            writeEpicTaskToDB(task);
+            writeEpicTaskToDB((EpicTask) task);
+        } else {
+            writeSubTaskToDB((SubTask) task);
         }
     }
 
     void writeTaskToDB(AbstractTask task) throws IOException {
-        try (Writer fileWriter = new FileWriter(DATABASE_FILE, true)) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(TaskType.TASK.toString());
-            stringBuilder.append(",");
-            stringBuilder.append(task.getId());
-            stringBuilder.append(",");
-            stringBuilder.append(",");
-            stringBuilder.append(task.getName());
-            stringBuilder.append(",");
-            stringBuilder.append(task.getDescription());
-            stringBuilder.append(",");
-            stringBuilder.append(task.getStatus());
-            stringBuilder.append(",");
-            stringBuilder.append("null");
-            stringBuilder.append(",");
-            stringBuilder.append("null");
-
-
-            fileWriter.write(stringBuilder.toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        StringBuilder stringBuilder = buildBasicString(task);
+        stringBuilder.append(",");
+        stringBuilder.append("null");
+        stringBuilder.append(",");
+        stringBuilder.append("null");
+        writeToDB(stringBuilder);
     }
 
-    void writeEpicTaskToDB(AbstractTask task) throws IOException {
+    void writeEpicTaskToDB(EpicTask task) throws IOException {
+        StringBuilder stringBuilder = buildBasicString(task);
+        stringBuilder.append(",");
+        stringBuilder.append(task.getSubTasks().toString());
+        stringBuilder.append(",");
+        stringBuilder.append("null");
+        writeToDB(stringBuilder);
+    }
+
+    void writeSubTaskToDB(SubTask task) throws IOException {
+        StringBuilder stringBuilder = buildBasicString(task);
+        stringBuilder.append(",");
+        stringBuilder.append("null");
+        stringBuilder.append(",");
+        stringBuilder.append(task.getEpicTask().toString());
+        writeToDB(stringBuilder);
+    }
+
+    void writeToDB(StringBuilder stringBuilder) throws IOException {
         try (Writer fileWriter = new FileWriter(DATABASE_FILE, true)) {
-            StringBuilder stringBuilder = buildBasicString(task);
-
-            stringBuilder.append(",");
-            stringBuilder.append("null");
-            stringBuilder.append(",");
-            stringBuilder.append("null");
-
             fileWriter.write(stringBuilder.toString());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
