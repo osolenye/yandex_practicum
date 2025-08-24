76a8cad694d0e1de9dd627fa7fdbcf4baea28681
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-08-24 13:33:34 +0600

    remaking the file write func

--- src/main/java/sprint5/final_project/FileBackedTaskManager.java
+++ src/main/java/sprint5/final_project/FileBackedTaskManager.java
@@ -1,7 +1,13 @@
 package sprint5.final_project;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 // instead of copying code from the class just inherit from it and get all code via DRY
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager{
+    final String DATABASE_FILE = "/home/osolenye/programming/yandex_practicum/src/main/java/sprint5/final_project/database.csv";
     @Override
     public void example() {
         super.example();
@@ -78,7 +84,66 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
     }
 
 
-    public void save(AbstractTask task) {
+    public void save(AbstractTask task) throws IOException {
+        if (task.getClass() == Task.class) {
+            writeTaskToDB(task);
+        } else if (task.getClass() == EpicTask.class) {
+            writeEpicTaskToDB(task);
+        }
+    }
 
+    void writeTaskToDB(AbstractTask task) throws IOException {
+        try (Writer fileWriter = new FileWriter(DATABASE_FILE, true)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(TaskType.TASK.toString());
+            stringBuilder.append(",");
+            stringBuilder.append(task.getId());
+            stringBuilder.append(",");
+            stringBuilder.append(",");
+            stringBuilder.append(task.getName());
+            stringBuilder.append(",");
+            stringBuilder.append(task.getDescription());
+            stringBuilder.append(",");
+            stringBuilder.append(task.getStatus());
+            stringBuilder.append(",");
+            stringBuilder.append("null");
+            stringBuilder.append(",");
+            stringBuilder.append("null");
+
+
+            fileWriter.write(stringBuilder.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void writeEpicTaskToDB(AbstractTask task) throws IOException {
+        try (Writer fileWriter = new FileWriter(DATABASE_FILE, true)) {
+            StringBuilder stringBuilder = buildBasicString(task);
+
+            stringBuilder.append(",");
+            stringBuilder.append("null");
+            stringBuilder.append(",");
+            stringBuilder.append("null");
+
+            fileWriter.write(stringBuilder.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    StringBuilder buildBasicString(AbstractTask task) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(TaskType.TASK.toString());
+        stringBuilder.append(",");
+        stringBuilder.append(task.getId());
+        stringBuilder.append(",");
+        stringBuilder.append(",");
+        stringBuilder.append(task.getName());
+        stringBuilder.append(",");
+        stringBuilder.append(task.getDescription());
+        stringBuilder.append(",");
+        stringBuilder.append(task.getStatus());
+        return stringBuilder;
     }
 }
