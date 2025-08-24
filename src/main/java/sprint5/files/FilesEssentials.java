602682b5c81df7e3d90cde0147824f8d2abce164
Raiimkulov Bekzat <raiimkulov.bekzat@gmail.com>
2025-08-24 17:30:44 +0600

    pull from git

--- src/main/java/sprint5/files/FilesEssentials.java
+++ src/main/java/sprint5/files/FilesEssentials.java
@@ -4,7 +4,27 @@ import java.io.File;
 
 public class FilesEssentials {
     public static void main(String[] args) {
-        File dir = new File("/home/osolenye/programming/yandex_practicum/src/main/java/sprint5/files");
+        // it is in charge of
+        File dir = new File("D:\\programming\\yandex_practicum\\src\\main\\java\\sprint5\\files");
+        // using a dir as a parent folder and other files will be the child files of the folder
+        File testTxt = new File(dir, "test.txt");
+
+        // check if the parent folder even exists
+        if (dir.isDirectory()) {
+            for (File item : dir.listFiles()) {
+                if (item.isDirectory()) {
+                    System.out.println("dir's name: " + item.getName());
+                } else if (item.isFile()) {
+                    System.out.println("file's name: " + item.getName());
+                }
+            }
+        }
+
+        // boolean mkdir() method creates a dir
+        // boolean renameTo obvious
+        // boolean delete() obvoius
+
+
 
     }
 }
