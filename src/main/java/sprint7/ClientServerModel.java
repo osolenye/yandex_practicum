378f00ef3032abbead477d3c305e2d562a21cffe
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-08-27 18:21:43 +0600

    beginning of network

---
+++ src/main/java/sprint7/ClientServerModel.java
@@ -0,0 +1,13 @@
+package sprint7;
+
+public class ClientServerModel {
+    public static void main(String[] args) {
+        // network protocols are set of rules that are used to exchange data via networks
+        // these protocols are divided by networking stack - each of them is in charge of doing something
+        // most common network protocols are:
+        // Internet Protocol (IP) - prepares data to sending and sends it to a receiver
+        // Transmission Control Protocol - checks if IP has sent all data to the receiver, if not - sends again.
+        // HyperText Transfer Protocol - uses IP and TCP and gives client-server model - e.g. browser receiving HTML page
+        //
+    }
+}
