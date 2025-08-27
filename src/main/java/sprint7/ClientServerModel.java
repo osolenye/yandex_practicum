bd50b238bde560863c7073e4e20ebcd446658ef4
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-08-27 19:14:56 +0600

    http request structure

--- src/main/java/sprint7/ClientServerModel.java
+++ src/main/java/sprint7/ClientServerModel.java
@@ -2,12 +2,21 @@ package sprint7;
 
 public class ClientServerModel {
     public static void main(String[] args) {
+        // BASICS
         // network protocols are set of rules that are used to exchange data via networks
         // these protocols are divided by networking stack - each of them is in charge of doing something
         // most common network protocols are:
         // Internet Protocol (IP) - prepares data to sending and sends it to a receiver
         // Transmission Control Protocol - checks if IP has sent all data to the receiver, if not - sends again.
         // HyperText Transfer Protocol - uses IP and TCP and gives client-server model - e.g. browser receiving HTML page
-        //
+
+        // URL AND HTML
+        // http://info.cern.ch/hypertest/WWW/TheProject.html
+        // URL starts with protocol e.g. http
+        // server's name that gets the request e.g. info.cern.ch - via DNS this name is now associated with the server's IP address
+        // other is the directory of the page
+
+        // HTTP request structure
+
     }
 }
