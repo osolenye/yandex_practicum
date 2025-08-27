e4b36fe573b5d8cc5d671dcce30852437339a99f
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-08-27 17:07:29 +0600

    6 module needs to be trained bruh

---
+++ src/main/java/sprint6/unit_tests/BoundarieValuesAndEquivalenceClasses.java
@@ -0,0 +1,14 @@
+package sprint6.unit_tests;
+
+import java.lang.reflect.Executable;
+
+public class BoundarieValuesAndEquivalenceClasses {
+    public static void main(String[] args) {
+        // tests can take forever to run so we need to reduce them as much as possible but keeping them to test every value
+        // boundary values - parameters that if we pass them program's behaviour changes
+        // equivalence classes - parameters that don't change program's behaviour
+        // we should test program on every boundary and one value from within boundaries
+        // or every boundary and +/- 1 from the boundary
+
+    }
+}
--- src/main/java/sprint6/unit_tests/TestEnvironment.java
+++ src/main/java/sprint6/unit_tests/TestEnvironment.java
@@ -3,6 +3,7 @@ package sprint6.unit_tests;
 public class TestEnvironment {
     public static void main(String[] args) {
         // three environemts - dev, test, prod - a dedicated servers or stands for repository of deving testing or production
-
+        // sometimes one test has to be executed before other tests, and tests can execute in random order
+        // to make one test to always go first you should put @BeforeEach annotation above
     }
 }
