b07e083a3a867ac84046c10d7555ab006e21e4e7
Bekzat Raiimkulov <raiimkulov.bekzat@gmail.com>
2025-08-27 19:32:09 +0600

    a bit of networks

--- src/main/java/sprint7/ClientServerModel.java
+++ src/main/java/sprint7/ClientServerModel.java
@@ -17,6 +17,17 @@ public class ClientServerModel {
         // other is the directory of the page
 
         // HTTP request structure
-
+        // first line of client is the method (what the server should do)
+        // server requests with status code
+        // methods are get post put patch delete
+        // server's responses - 200 - 299 are success codes
+        // client's error - 400 - 499 errors
+        // server side error - 500-599
+        // headers:
+        // accept - what data type the client can accept
+        // host - server's address that the client is requesting to
+        // content-length - requests body size
+        // content-type - which type of data is being transferred
+        // the body can be any. Most popular: json, file etc
     }
 }
